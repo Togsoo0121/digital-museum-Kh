51d424f1996ccaaf85c623d8351b88bdcaf49cc6
Togsoo0121 <Bayrsurentogsoo@gmail.com>
2026-04-27 03:23:22 +0800

    Update import path for qpayService

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect, useId, useMemo, useState } from "react";
 import { db, auth } from "./firebase";
-import { createQPayInvoice } from "./services/qpayService";
+import { createQPayInvoice } from "./services/qpayService.js";
 import { qrSvgDataUrl } from "./utils/qrCode";
 import { onValue, ref, set } from "firebase/database";
 import {
